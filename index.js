9cff1426109e09502216be35b6b374d63ed317e8
st097 <s.tozaj@gmail.com>
2023-08-18 13:05:54 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings={
+    databaseURL: "https://playgroud-61a99-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app= initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function(){
+    let inputValue = inputFieldEl.value
+
+   push(shoppingListInDB, inputValue)
+
+   clearInputFieldEl()
+   appendItemToShoppingListEL(inputValue)
+  
+ 
+})
+
+function clearInputFieldEl(){
+
+    inputFieldEl.vlue=""
+}
+
+function appendItemToShoppingListEL(itemValue){
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
