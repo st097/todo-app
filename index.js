e4204db24c4cb1bf6a6b1a2d0b1b85e30b59bce7
st097 <s.tozaj@gmail.com>
2023-08-30 22:38:50 +0200

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings={
     databaseURL: "https://playgroud-61a99-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -23,6 +23,11 @@ addButtonEl.addEventListener("click", function(){
    appendItemToShoppingListEL(inputValue)
   
  
+})
+
+onValue(shoppingListInDB, function(snapshot){
+let itemsArray = Object.values(snapshot.val())
+
 })
 
 function clearInputFieldEl(){
